bbbf23dc33090980631deaa2c0cd01e04778edba
Colin <cabb007@live.com>
2026-03-18 17:13:23 -0500

    Express Server setup
    initial setup of Express Server, connecting using same localhost port. added 'proxy' line for easier database integration later

---
+++ client/server.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/api/data', (req, res) => {
+    res.json({
+        message: 'Hello from Express!'
+    })
+})
+
+const PORT = 5173;
+app.listen(PORT, () => console.log('Server running on port ' + PORT));
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,18 @@
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Landing from "./pages/Landing";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 
 export default function App() {
+  const [message, setMessage] = useState('')
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/api/data')
+        .then(response => setMessage(response.data.message))
+        .catch(error => console.error(error));
+  }, []);
   return (
     <BrowserRouter>
       <Routes>
