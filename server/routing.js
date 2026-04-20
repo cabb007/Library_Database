f671a7c467f491a2ae24a2a11312161a71aae14b
cabb007 <cabb007@live.com>
2026-04-20 05:02:50 -0500

    user session deployment bugfix 2

--- server/routing.js
+++ server/routing.js
@@ -87,7 +87,7 @@ function getSession(req, res) {
 
   res.setHeader(
     "Set-Cookie",
-    `sid=${newSid};Path=/; SameSite=none; Secure; Max-Age: 1000 * 60 * 60 * 24`
+    `sid=${newSid}; Path=/; SameSite=None; Secure; HttpOnly; Max-Age=${60 * 60 * 24}`
   );
 
   return session;
